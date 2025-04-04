6f2b4793c47cbd750fb15962f460275867915625
Nelson <punkpunkada@gmail.com>
2025-04-04 16:16:41 -0300

    testing @for @if in Angular17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,39 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'learnAngular17';
+
+  userDatasList: Array<{
+    id: number;
+    name: string;
+    age: number;
+    profession: string;
+  }> = [{
+    id: 1,
+    name: 'John Doe',
+    age: 30,
+    profession: 'Software Engineer'
+  }, {
+    id: 2,
+    name: 'Jane Smith',
+    age: 25,
+    profession: 'Data Scientist'
+  }, {
+    id: 3,
+    name: 'Alice Johnson',
+    age: 28,
+    profession: 'Product Manager'
+  },
+{
+    id: 4,
+    name: 'Bob Brown',
+    age: 35,
+    profession: 'UX Designer'
+  }, {
+    id: 5,
+    name: 'Charlie Davis',
+    age: 32,
+    profession: 'DevOps Engineer'
+}]
+
+
 }
